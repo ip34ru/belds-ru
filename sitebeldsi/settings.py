87ec993a9ee16ef86b922f5dd713f82ebe789dee
root <root@belds.ru>
2016-12-21 12:40:27 -0500

    Change config

--- sitebeldsi/settings.py
+++ sitebeldsi/settings.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-
+ADMINS = (
+     ('TAksenov', 'taksenov@gmail.com'),
+)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.conf import settings
